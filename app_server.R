9329ed97553b7df9e99cdb026903679997fb42df
Maynard Maynard-Zhang <>
2021-07-28 20:33:45 -0700

    Add line graph to Shiny app

--- app_server.R
+++ app_server.R
@@ -1,3 +1,21 @@
 server <- function(input, output) {
+    # Vis 1 - Line
+    output$line <- renderPlot({
+        # Plot
+        line.plot <- ggplot(data=farms_monthly) +
+            geom_line(
+                mapping = aes(x = order_month,
+                              y = num_farms_cum),
+                size = 2
+            ) +
+            labs(
+                title = "Farms Served Monthly",
+                x = "Date",
+                y = "Number of Farms"
+            )
 
+        return(line.plot)
+    })
+
+    # Vis 2 - Map
 }
--- app_ui.R
+++ app_ui.R
@@ -1,3 +1,27 @@
+# Vis 1 - Line
+# line_data <- selectInput(
+#     inputId = "line_data",
+#     label = "Select what data to display:",
+#     choices = c(
+#         farms_monthly
+#     ),
+#     selected="Total farms served"
+# )
+
+line_panel <- tabPanel(
+    "Line Graph",
+    # sidebarPanel(
+    #     line_data
+    # ),
+    mainPanel(
+        plotOutput("line")
+    )
+)
+
+# Vis 2 - Map
+
+# UI
 ui <- navbarPage(
-    "King County Farmers Share"
+    "King County Farmers Share",
+    line_panel
 )
