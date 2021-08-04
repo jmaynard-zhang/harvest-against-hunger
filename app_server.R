1ca60702de0b4d71915f28c29166dbcfbaa9c7d1
Maynard Maynard-Zhang <>
2021-08-04 10:37:18 -0700

    Add interactive map and data selection widget

--- app_server.R
+++ app_server.R
@@ -2,18 +2,18 @@ server <- function(input, output) {
     # Vis 1 - Line
     output$line <- renderPlotly({
         # Select data widget
-        data <- get(input$line_data)
+        line_data <- get(input$line_select)
         y_col <- colnames(data)[[3]]
 
         # Plot
-        line.plot <- ggplot(data=data) +
+        line.plot <- ggplot(data=line_data) +
             geom_line(
                 mapping = aes(x = order_month,
-                              y = data[[3]]),
+                              y = line_data[[3]]),
                 size = 2
             ) +
             labs(
-                title = input$line_data,
+                title = input$line_select,
                 x = "Date",
                 y = "Number of Farms"
             )
@@ -22,4 +22,22 @@ server <- function(input, output) {
     })
 
     # Vis 2 - Map
+    output$map <- renderPlotly({
+        # Select data widget
+        map_data <- get(input$map_select)
+
+        # Plot
+        map.plot <- ggplot() +
+            geom_polygon(data=wa,
+                         aes(x=long, y=lat, group=group),
+                         alpha=0.3) +
+            geom_point(data=map_data,
+                       fill = "grey",
+                       aes(x=map_data$lon,
+                           y=map_data$lat,
+                           alpha=map_data[[4]])) +
+            theme_void() +
+            coord_map() +
+            ggtitle("Total Orders since 2019")
+    })
 }
--- app_ui.R
+++ app_ui.R
@@ -1,20 +1,19 @@
 # Vis 1 - Line
-line_data <- selectInput(
-    inputId = "line_data",
+line_select <- selectInput(
+    inputId = "line_select",
     label = "Select what data to display:",
     choices = c(
         "farms_monthly",
         "orders_monthly",
         "dollars_monthly",
         "lbs_monthly"
-    ),
-    selected="farms_monthly"
+    )
 )
 
 line_panel <- tabPanel(
     "Line Graph",
     sidebarPanel(
-        line_data
+        line_select
     ),
     mainPanel(
         plotlyOutput("line")
@@ -22,9 +21,29 @@ line_panel <- tabPanel(
 )
 
 # Vis 2 - Map
+map_select <- selectInput(
+    inputId = "map_select",
+    label = "Select what data to display:",
+    choices = c(
+        "total_orders",
+        "total_dollars",
+        "total_lbs"
+    )
+)
+
+map_panel <- tabPanel(
+    "Map Graph",
+    sidebarPanel(
+        map_select
+    ),
+    mainPanel(
+        plotlyOutput("map")
+    )
+)
 
 # UI
 ui <- navbarPage(
     "King County Farmers Share",
-    line_panel
+    line_panel,
+    map_panel
 )
